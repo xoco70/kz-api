6216a8df25211d99fc841417d91def67a57db92b
Julien Cappiello <contact@kendozone.com>
2018-06-22 13:59:42 +0200

    refactor country seed

--- database/seeds/CountriesSeeder.php
+++ database/seeds/CountriesSeeder.php
@@ -21,25 +21,27 @@ class CountriesSeeder extends Seeder {
         $countries = new Countries();
 
         $countries = $countries->getList();
+
+        //TODO Refactor it
         foreach ($countries as $countryId => $country){
             DB::table(config('countries.table_name'))->insert(array(
                 'id' => $countryId,
-                'capital' => ((isset($country['capital'])) ? $country['capital'] : null),
-                'citizenship' => ((isset($country['citizenship'])) ? $country['citizenship'] : null),
-                'country_code' => $country['country-code'],
-                'currency' => ((isset($country['currency'])) ? $country['currency'] : null),
-                'currency_code' => ((isset($country['currency_code'])) ? $country['currency_code'] : null),
-                'currency_sub_unit' => ((isset($country['currency_sub_unit'])) ? $country['currency_sub_unit'] : null),
-                'full_name' => ((isset($country['full_name'])) ? $country['full_name'] : null),
-                'iso_3166_2' => $country['iso_3166_2'],
-                'iso_3166_3' => $country['iso_3166_3'],
-                'name' => $country['name'],
-                'region_code' => $country['region-code'],
-                'sub_region_code' => $country['sub-region-code'],
-                'eea' => (bool)$country['eea'],
-                'calling_code' => $country['calling_code'],
-                'currency_symbol' => ((isset($country['currency_symbol'])) ? $country['currency_symbol'] : null),
-                'flag' =>((isset($country['flag'])) ? $country['flag'] : null),
+                'capital' => $country['capital'] ?? null,
+                'citizenship' => $country['citizenship'] ?? null,
+                'country_code' => $country['country_code'] ?? null,
+                'currency' => $country['currency'] ?? null,
+                'currency_code' => $country['currency_code'] ?? null,
+                'currency_sub_unit' => $country['currency_sub_unit'] ?? null,
+                'full_name' => $country['full_name'] ?? null,
+                'iso_3166_2' => $country['iso_3166_2'] ?? null,
+                'iso_3166_3' => $country['iso_3166_3'] ?? null,
+                'name' => $country['name'] ?? null,
+                'region_code' => $country['region_code'] ?? null,
+                'sub_region_code' => $country['sub_region_code'] ?? null,
+                'eea' => $country['eea'] ?? null,
+                'calling_code' => $country['calling_code'] ?? null,
+                'currency_symbol' => $country['currency_symbol'] ?? null,
+                'flag' => $country['flag'] ?? null,
             ));
         }
 
