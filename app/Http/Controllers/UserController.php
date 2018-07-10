3bff6e0d96d49a5a725d7818134c50c15f5827a8
Julien Cappiello <contact@kendozone.com>
2018-07-10 16:54:39 +0200

    add avatar upload stage 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Association;
-use App\Club;
-use App\Federation;
 use App\Http\Resources\UserResource;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 
 class UserController extends Controller
@@ -18,19 +18,12 @@ class UserController extends Controller
         return UserResource::collection(User::paginate(25));
     }
 
-    public function edit(Request $request, $slug)
+    public function edit($slug)
     {
         try {
-            return response()->json(
-                [
-                    'user' => User::where('slug', $slug)->first(),
-                    'federations' => Federation::all(),
-                    'associations' => Association::all(),
-                    'clubs' => Club::all(),
-                ], 200
-            );
+            return response()->json(User::where('slug', $slug)->first(), Response::HTTP_OK);
         } catch (\Exception $e) {
-            return response()->json($e->getMessage(), $e->getCode());
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -43,6 +36,46 @@ class UserController extends Controller
 
     public function update(Request $request, $slug)
     {
+        try {
+            $this->validate($request, [
+                'name' => 'required',
+            ]);
+            $user = User::find($request->id);
+            $user->name = $request->name;
+            $user->firstname = $request->firstname;
+            $user->lastname = $request->lastname;
+            $user->country_id = $request->country_id ?? null;
+            $user->grade_id = $request->grade_id  ?? null;
+            $user->update();
 
+            return $user;
+
+        } catch (ValidationException $e) {
+            return response()->json($e->getMessage(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function upload(Request $request)
+    {
+//        $request->validate([
+//            'file' => ['required', 'image']
+//        ]);
+        $file = $request
+            ->file('file')
+            ->store('images/avatar', 'public');
+
+//        Image::make(storage_path('app/public/' . $file))
+//            ->resize(200, 200, function ($constraint) {
+//                $constraint->aspectRatio();
+//                $constraint->upsize();
+//            })
+//            ->save();
+
+//        Auth::user()->update([
+//            'avatar' => basename($file)
+//        ]);
+        return response([], 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,15 @@ $router->get('register/confirm/{token}', 'Auth\RegisterController@confirm');
 $router->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 //$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
-$router->get('/tournaments/{slug}/fights', 'FightController@index');
 $router->group(['middleware' => 'jwt.auth'],
     function () use ($router) {
         $router->get('/tournaments', 'TournamentController@index');
-        $router->get('/tournaments/{slug}/edit', 'TournamentController@edit');
         $router->delete('/tournaments/{slug}', 'TournamentController@destroy');
         $router->post('/tournaments', 'TournamentController@store');
+        $router->get('/tournaments/{slug}/edit', 'TournamentController@edit');
         $router->put('/tournaments/{slug}', 'TournamentController@update');
-        $router->get('/tournaments/levels', 'TournamentLevelController@index');
-        $router->get('/tournaments/presets', 'PresetsController@index');
+
+        $router->get('/tournaments/{slug}/statistics', 'TournamentController@statistics');
 
         $router->get('/categories', 'CategoryController@index');
         $router->post('/categories', 'CategoryController@store');
@@ -32,10 +31,13 @@ $router->group(['middleware' => 'jwt.auth'],
         $router->get('/tournaments/{slug}/trees', 'TreeController@index');
         $router->post('/championships/{championshipId}/trees', 'TreeController@store');
 
+        $router->get('/tournaments/{slug}/fights', 'FightController@index');
+
         $router->get('/users', 'UserController@index');
         $router->get('/users/{slug}/edit', 'UserController@edit');
-        $router->get('/users/{slug}', 'UserController@post');
+        $router->put('/users/{slug}', 'UserController@update');
     }
 );
+$router->post('/user/avatar', 'UserController@upload');
 
 
