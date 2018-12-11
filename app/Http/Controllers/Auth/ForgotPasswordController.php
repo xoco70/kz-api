399fa4b693989791ab7fe0ec18f2dc6e2b846cc1
Cappiello Julien <julien@cappiello.fr>
2018-12-11 16:23:14 +0100

    change format when error on reseting pass

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Notifications\ResetLinkEmailSent;
 use App\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ForgotPasswordController extends Controller
 {
@@ -32,13 +35,20 @@ class ForgotPasswordController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $user = User::where('email', $request->email)->firstOrFail();
-        $token = str_random(64);
-        DB::table(config('auth.passwords.users.table'))->insert([
-            'email' => $user->email,
-            'token' => $token
-        ]);
-        $user->notify(new ResetLinkEmailSent($user, $token));
+        try {
+            $user = User::where('email', $request->email)->firstOrFail();
 
+            $token = str_random(64);
+            DB::table(config('auth.passwords.users.table'))->insert([
+                'email' => $user->email,
+                'token' => $token
+            ]);
+            $user->notify(new ResetLinkEmailSent($user, $token));
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        } catch (\Exception $e) {
+            return response()->json(['class' => $e->getTraceAsString(), 'message'=> $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return null;
     }
 }
